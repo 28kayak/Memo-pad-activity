1f95d54f98c27d11e31ba9c3862c8df1eead2a1e
kaya <kaya_ota@msn.com>
2013-01-05 22:18:27 -0800

    initial commit for MemoList

---
+++ src/com/example/memopad/MemoList.java
@@ -0,0 +1,11 @@
+package com.example.memopad;
+
+import android.app.ListActivity;
+
+public class MemoList extends ListActivity {
+
+	public MemoList() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
