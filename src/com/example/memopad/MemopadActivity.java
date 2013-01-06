63ff7e5c119778bf3782d7f22c9b9488c4919a69
kaya <kaya_ota@msn.com>
2013-01-05 21:01:54 -0800

    test of git branch

--- src/com/example/memopad/MemopadActivity.java
+++ src/com/example/memopad/MemopadActivity.java
@@ -3,11 +3,17 @@ package com.example.memopad;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-
 //import statement for onStop()
 import android.content.SharedPreferences;//interface
 import android.text.Selection;
 import android.widget.EditText;
+//imports for saveMemo()
+import java.text.DateFormat;
+import java.util.Date;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+
+//testing branch git
 
 public class MemopadActivity extends Activity
 {
@@ -54,4 +60,36 @@ public class MemopadActivity extends Activity
 		getMenuInflater().inflate(R.menu.activity_memopad, menu);
 		return true;
 	}
+	
+	void saveMemo()
+	{
+		EditText et = (EditText) this.findViewById(R.id.editText);
+		String title;
+		String memo = et.getText().toString();
+		
+		if(memo.trim().length() > 0)
+		{
+			if(memo.indexOf("\n")==-1)
+			{
+				title = memo.substring(0, Math.min(memo.length(), 20));
+				
+			}
+			else
+			{
+				title = memo.substring(0, Math.min(memo.indexOf("\n"), 20));
+			}
+		
+		String ts = DateFormat.getDateTimeInstance().format(new Date());
+		MemoDBHelper memos = new MemoDBHelper(this);
+		SQLiteDatabase db = memos.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put("title", title + "\n" + ts);
+		values.put("memo", memo);
+		
+		
+		
+		}//if
+		
+		
+	}//saveMemo()
 }
