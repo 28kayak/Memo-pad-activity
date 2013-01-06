fb623921052d76d379203d07915a2cfb7753c2c5
kaya <kaya_ota@msn.com>
2013-01-05 17:37:33 -0800

    add onStop method to save written data on memopad application

--- src/com/example/memopad/MemopadActivity.java
+++ src/com/example/memopad/MemopadActivity.java
@@ -1,10 +1,18 @@
 package com.example.memopad;
-
+//initial memo app which can temporally write data on the memo-pad apps
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-//initial memo app which can temporally write data on the memopad apps
-public class MemopadActivity extends Activity {
+
+//import statement for onStop()
+import android.content.SharedPreferences;//interface
+import android.text.Selection;
+import android.widget.EditText;
+
+public class MemopadActivity extends Activity
+{
+
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -12,8 +20,29 @@ public class MemopadActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_memopad);
 	}
-
 	
+	//onStop method is called before app shuts down, 
+	//so it contains the function which saves written info
+	@Override
+	protected void onStop()
+	{
+		EditText et = (EditText) findViewById(R.id.editText);
+		//use method findViewById and contain an object of EditText into the variable et 
+		SharedPreferences pref = getSharedPreferences("MemoPref", MODE_PRIVATE);
+		//create an object of SharedPreferences using getSharedPreferences
+		SharedPreferences.Editor  editor = pref.edit();
+		//SharedPreferences.Editor used to write SharedPreferences gets from edit().
+		editor.putString("memo", et.getText().toString());
+		//canNOT save Editable type,so change to String type by toString() 
+		editor.putInt("cursor", Selection.getSelectionStart(et.getText()));
+		//contain putString() gets String and putText()gets int
+		editor.commit();
+		//save data and done
+		
+		super.onStop();
+	}
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
